df1ee601f7d977fe4426b1c06fa10b58672c431b
Reeba Patel <129683467+ReebaPatel@users.noreply.github.com>
2024-06-18 16:20:28 +0530

    [new commit]

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
+import styles from "./Contact.module"
 import { getImageUrl } from '../../utils'
 
 const Contact = () => {
     return (
-        <footer>
-            <div>
+        <footer id='contact' className={styles.container}>
+            <div className={styles.text}>
                 <h2>Contact</h2>
                 <p>Feel free to reach out</p>
             </div>
-            <ul>
-                <li>
+            <ul className={styles.links} >
+                <li className={styles.link}>
                     <img src={getImageUrl("contact/email.png")} alt="email icon " />
                     <a href="mailto:reebapatel2210@gmail.com">reebapatel2210@gmail.com</a>
                 </li>
-                <li>
+                <li className={styles.link}>
                     <img src={getImageUrl("contact/linkedin.png")} alt="linkedin icon " />
                     <a href="linkedin.com/in/reeba-patel-981b50290">linkedin.com/in/reeba-patel-981b50290</a>
                 </li>
-                <li>
+                <li className={styles.link}>
                     <img src={getImageUrl("contact/github.png")} alt="github icon " />
                     <a href="mailto:reebapatel2210@gmail.com">reebapatel2210@gmail.com</a>
                 </li>
